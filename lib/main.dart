78d898f49ed71417696cb0a61f91ccb6c789f000
Coding Ninja <cn.codingninja@gmail.com>
2024-11-04 18:49:14 +0500

    Created Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -11,8 +12,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int count = 0;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Center(child: Text("My Counter App",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 25),)),
+            backgroundColor: Colors.deepOrangeAccent,
+          ),
+          body: Center(
+              child: Text(
+            count.toString(),
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          )),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              count++;
+              setState(() {});
+            },
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.deepOrange,
+            child: Icon(Icons.add),
+          ),
+        ),
+      ),
+    );
   }
 }
